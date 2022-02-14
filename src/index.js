262efc1d2b967420cbed692287cce3d7ff61c3c8
Jehangir Wahid <magnificentknight@gmail.com>
2022-02-14 15:58:48 +0400

    Allowing all methods.

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use(
 app.use(cookieParser());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, *"
