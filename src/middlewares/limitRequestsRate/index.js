55267c6235e13624388f0a268da900d1fb5dd348
Jehangir Wahid <magnificentknight@gmail.com>
2022-02-16 04:53:16 +0400

    Increasing requests rate limit from 100 to 200

--- src/middlewares/limitRequestsRate/index.js
+++ src/middlewares/limitRequestsRate/index.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
     windowMs: 20 * 60 * 1000,
-    max: 100,
+    max: 200,
     standardHeaders: true,
     legacyHeaders: false,
     handler: function (req, res) {
